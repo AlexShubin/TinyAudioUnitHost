8de420e5069b6529e127e0b2a1d1276d937ebfac
Alex Shubin <alexshubin22@gmail.com>
2026-04-20 14:06:11 +0200

    resizable window

--- Sources/Features/Host/Subviews/AudioUnitView.swift
+++ Sources/Features/Host/Subviews/AudioUnitView.swift
@@ -14,14 +14,14 @@ struct AudioUnitView: NSViewControllerRepresentable {
     let audioUnit: AUAudioUnit
     let onSizeChange: (CGSize) -> Void
 
-    func makeNSViewController(context: Context) -> AudioUnitContainerViewController {
-        let container = AudioUnitContainerViewController()
-        container.audioUnit = audioUnit
-        container.onSizeChange = onSizeChange
-        return container
+    func makeNSViewController(context: Context) -> AudioUnitHostViewController {
+        let host = AudioUnitHostViewController()
+        host.audioUnit = audioUnit
+        host.onSizeChange = onSizeChange
+        return host
     }
 
-    func updateNSViewController(_ controller: AudioUnitContainerViewController, context: Context) {
+    func updateNSViewController(_ controller: AudioUnitHostViewController, context: Context) {
         controller.onSizeChange = onSizeChange
         if controller.audioUnit !== audioUnit {
             controller.audioUnit = audioUnit
@@ -30,10 +30,15 @@ struct AudioUnitView: NSViewControllerRepresentable {
     }
 }
 
-final class AudioUnitContainerViewController: NSViewController {
+final class AudioUnitHostViewController: NSViewController {
     var audioUnit: AUAudioUnit?
     var onSizeChange: ((CGSize) -> Void)?
     private var auViewController: NSViewController?
+    private var sizeObservation: NSKeyValueObservation?
+
+    override func loadView() {
+        view = NSView()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,8 +46,9 @@ final class AudioUnitContainerViewController: NSViewController {
     }
 
     func loadAudioUnitView() {
+        sizeObservation?.invalidate()
+        sizeObservation = nil
         auViewController?.view.removeFromSuperview()
-        auViewController?.removeFromParent()
         auViewController = nil
 
         guard let audioUnit else { return }
@@ -56,7 +62,6 @@ final class AudioUnitContainerViewController: NSViewController {
     }
 
     private func install(viewController: NSViewController) {
-        addChild(viewController)
         let auView = viewController.view
         auView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(auView)
@@ -71,7 +76,19 @@ final class AudioUnitContainerViewController: NSViewController {
         auViewController = viewController
 
         let preferred = viewController.preferredContentSize
-        let size = (preferred.width > 0 && preferred.height > 0) ? preferred : auView.fittingSize
-        onSizeChange?(size)
+        let initialSize = (preferred.width > 0 && preferred.height > 0) ? preferred : auView.fittingSize
+        onSizeChange?(initialSize)
+
+        sizeObservation = viewController.observe(\.preferredContentSize, options: [.new]) { [weak self] vc, _ in
+            let size = vc.preferredContentSize
+            guard size.width > 0 && size.height > 0 else { return }
+            DispatchQueue.main.async {
+                self?.onSizeChange?(size)
+            }
+        }
+    }
+
+    deinit {
+        sizeObservation?.invalidate()
     }
 }
